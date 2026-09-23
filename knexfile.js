b82bb4a77d2a809f7b5498d59501d50e357f63b2
vutrongtuyen2401 <phucvucongviec1234hs@gmail.com>
2026-09-23 22:26:59 +0700

    feat: add users roles and demo seed

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
-    module.exports = {
-      development: {
-        client: 'pg',
-        connection: process.env.DB_CONNECTION_STRING,
-        migrations: {
-          directory: './migrations',
-        },
-      },
-    };
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DB_CONNECTION_STRING,
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds',
+    },
+  },
+};
